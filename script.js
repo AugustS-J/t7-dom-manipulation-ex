84367e4425fe309708af46137d1cd11040507395
AugustS-J <ausc0001@stud.kea.dk>
2023-08-29 09:33:29 +0200

    Ændret JavaScript

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 //Write your own code here
 //DON'T touch any other files!
+
+//Remove all break tags
+document.querySelectorAll("br").forEach((elm) => elm.remove());
+
+//Give all sections the class "filled"
+document.querySelectorAll("section").forEach((elm) => elm.classList.add("filled"));
+
+//Remove all elements that come before a section
+document.querySelectorAll("section").forEach((elm) => elm.previousElementSibling.remove());
+
+//console.log(this);
